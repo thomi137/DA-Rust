001430e453c917a9bafac8697b5c7e9656217bf0
Thomas Prosser <tp@thomit.com>
2025-10-22 18:14:47 +0200

    A whole afternoon of bitter fights with the borrow checker

--- src/lib.rs
+++ src/lib.rs
@@ -125,23 +125,58 @@ pub mod linalg {
     /// * `upper_lower`: Upper or lower part of symmetric matrix not 0.
     /// * `n`: Matrix order. usize. We only need one rank, since we want a symmetric matrix.
     /// * `system_size`: width of the system
+    #[derive(Clone)]
     pub struct EigenConfig {
         pub jobz: u8,
         pub uplo: u8,
         pub n: i32,
         pub system_width: f64,
+        pub lda: i32,
+        pub lwork: i32
     }
     impl EigenConfig {
         pub fn init(job: Jobz, upper_lower: Uplo, step_number: usize, system_width: f64 ) -> EigenConfig {
             let jobz = get_jobz(job);
             let uplo = get_uplo(upper_lower);
             let n = step_number as i32;
+            let lda = n.clone();
+            let lwork = 3*n-1;
             let system_width = system_width;
 
-            EigenConfig { jobz, uplo, n, system_width }
+            EigenConfig { jobz, uplo, n, system_width, lwork, lda }
         }
     } // Impl Eigenconfig
 } // mod Linalg
+
+mod solvers {
+
+    use lapack::*;
+    use crate::linalg::{EigenConfig, Jobz};
+
+    fn eigensolver(config: &EigenConfig, hamiltonian: &Vec<f64>) -> Result<(Vec<f64>, Vec<f64>), &'static str> {
+
+        let cfg = config.clone();
+        let rng = cfg.n.clone() as usize;
+
+        let mut matrix = hamiltonian.clone();
+        let mut w = vec![0.0; rng];
+        let mut work = vec![0.0; rng];
+        let mut info = 2939;
+
+        unsafe {
+            dsyev(cfg.jobz, cfg.uplo, cfg.n, &mut matrix, cfg.lda, &mut w, &mut work, cfg.lwork, &mut info);
+        }
+
+        if info == 0 {
+           return if cfg.jobz ==  b'N' { Ok((w, vec![])) }
+            else if cfg.jobz == b'V' { Ok((w, matrix))}
+            else { Ok((vec![], vec![]))}
+        }
+
+        Err("Algorithm failed to converge: info is {info}, please consult https://www.netlib.org/lapack/explore-html-3.6.1/d2/d8a/group__double_s_yeigen_ga442c43fca5493590f8f26cf42fed4044.html")
+    }
+
+} // mod Solvers
 
 #[cfg(test)]
 mod tests {
