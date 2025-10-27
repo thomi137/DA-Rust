e05aef346ff96aa227effa994cb066fc1f7d3988
Thomas Prosser <tp@thomit.com>
2025-10-27 01:07:37 +0100

    and reporting

--- src/math/calculus.rs
+++ src/math/calculus.rs
@@ -1,6 +1,13 @@
 use fftw::*;
 use fftw::plan::{C2CPlan64};
 use fftw::types::c64;
+use num::Complex;
 
 type Direction = types::Sign;
 type Plan = C2CPlan64;
+
+fn fft(&n: usize, direction: Direction, plan: Plan, &input: Vec<c64> ) -> Result<Vec<c64>, error::Error> {
+
+
+    Ok(())
+}
